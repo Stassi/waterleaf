f0a2e23d7c6f9d6c27e90c0e1b52074f6d9c5172
Andreas Stassivik <andreas@stassi.net>
2020-01-01 20:07:32 -0800

    -erroneous logic

--- test/truth.js
+++ test/truth.js
@@ -3,7 +3,7 @@ import { expect } from 'chai'
 import { truth } from '../src'
 
 describe('#truth', () => {
-  it("isn't truth", () => {
-    expect(truth()).to.not.equal(true)
+  it('is truth', () => {
+    expect(truth()).to.equal(true)
   })
 })
