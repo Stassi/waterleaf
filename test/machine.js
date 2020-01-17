50d6e4a50f0a9bb2eb188d12fa0ef3fed20f99be
Andreas Stassivik <andreas@stassi.net>
2020-01-16 20:45:18 -0800

    machine test

---
+++ test/machine.js
@@ -0,0 +1,86 @@
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
+import { machine } from '../src'
+
+describe('#machine', () => {
+  const options = {
+    state: 's0',
+    states: {
+      s0: {
+        instruction: () => 'β',
+        move: () => 1,
+        nextState: 's1'
+        // nextState: () => 's1'
+      },
+      s1: {
+        instruction: () => 'α',
+        move: () => 1,
+        nextState: 's2'
+        // nextState: () => 's2'
+      },
+      s2: {
+        instruction: () => 'β',
+        move: () => 1,
+        nextState: 's3'
+        // nextState: () => 's3'
+      },
+      s3: {
+        instruction: () => 'α',
+        move: () => 0
+      }
+    },
+    symbolDefault: 'α',
+    tape: ['α']
+  }
+
+  describe('position: -2', () => {
+    const position = -2
+
+    it('needs a name', () => {
+      const res = machine({ position, ...options })
+      const res2 = res.step()
+      const res3 = res2.step()
+      const res4 = res3.step()
+
+      expect(res4).to.deep.include({
+        position: 3,
+        state: undefined,
+        tape: ['β', 'α', 'β', 'α']
+      })
+    })
+  })
+
+  describe('position: 0', () => {
+    const position = 0
+
+    it('needs a name', () => {
+      const res = machine({ position, ...options })
+      const res2 = res.step()
+      const res3 = res2.step()
+      const res4 = res3.step()
+
+      expect(res4).to.deep.include({
+        position: 3,
+        state: undefined,
+        tape: ['β', 'α', 'β', 'α']
+      })
+    })
+  })
+
+  describe('position: 2', () => {
+    const position = 2
+
+    it('needs a name', () => {
+      const res = machine({ position, ...options })
+      const res2 = res.step()
+      const res3 = res2.step()
+      const res4 = res3.step()
+
+      expect(res4).to.deep.include({
+        position: 5,
+        state: undefined,
+        tape: ['α', 'α', 'β', 'α', 'β', 'α']
+      })
+    })
+  })
+})
