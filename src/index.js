4ac84339c27f8252d447dd793a23429f1fcdb320
Andreas Stassivik <andreas@stassi.net>
2020-01-20 12:02:06 -0800

    -truth

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import machine from './machine'
 import print from './algorithms/print'
 import printReverse from './algorithms/printReverse'
-import truth from './truth'
 
 export {
   machine,
   print,
-  printReverse,
-  truth
+  printReverse
 }
--- test/truth.js
+++
@@ -1,9 +0,0 @@
-import { describe, it } from 'mocha'
-import { expect } from 'chai'
-import { truth } from '../src'
-
-describe('#truth', () => {
-  it('is truth', () => {
-    expect(truth()).to.equal(true)
-  })
-})
