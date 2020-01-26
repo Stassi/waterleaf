fbfd65dde32e6e5c4c469ee60a6be3a253e5466d
Andreas Stassivik <andreas@stassi.net>
2020-01-26 01:11:48 -0800

    @rollup/plugin-commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
+import commonjs from '@rollup/plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 
 const rollupConfig = [
